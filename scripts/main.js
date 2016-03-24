208c7e13fcf9b8934e298cbca72add29c2bf876e
Dimitry Solovyov <dimitry@solovyov.lv>
2016-03-24 11:28:15 +0200

    Footer

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+(function() {
+    var coords = [56.9086, 24.0829];
+    var map = L.map('map', {
+        scrollWheelZoom: false
+    }).setView(coords, 15);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    L.marker(coords).addTo(map)
+        .bindPopup('Vienības gatve 109, Rīga, LV-1058 <br> 6th floor, office 6.11.')
+        .openPopup();
+}());
