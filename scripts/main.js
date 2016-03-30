bc92a30b72285ad34b28dc34c14e2d3b7c2c6861
Dimitry Solovyov <dimitry@solovyov.lv>
2016-03-30 10:59:48 +0300

    OSM fix

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@
     var map = L.map('map', {
         scrollWheelZoom: false
     }).setView(coords, 15);
-    L.tileLayer('//{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
     L.marker(coords).addTo(map)
