7177a9af70439400cc527fcff2e07408723868d4
Dimitry Solovyov <dimitry@solovyov.lv>
2017-09-08 14:33:03 +0300

    Fix deploy script to remove old files

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,19 @@ echo "Deploying updates to GitHub..."
 
 echo "Removing old build..."
 rm -rf build
-mkdir build
 
 echo "Initializing git..."
 git clone -b master --single-branch git@github.com:neueda/neueda.github.io.git build
 
 echo "Building project..."
+rm -rf build/*
 hugo && cd build
 
 echo "Adding changes..."
 git add -A
 
 # Commit changes.
-msg="rebuilding site `date`"
+msg="Rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
